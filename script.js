86e93253f6ba3ab1600ffb953a174c0c2cc20190
sarunya <118037017+tunuk2004@users.noreply.github.com>
2023-03-13 01:14:05 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+let character = document.getElementById('character')
+let characterBottom = parseInt(window.getComputedStyle(character).getPropertyValue('bottom'));
+let characterRight = parseInt(window.getComputedStyle(character).getPropertyValue('right'));
+let characterWith = parseInt(window.getComputedStyle(character).getPropertyValue('width'));
+let ground = document.getElementById('ground');
+let groundBottom = parseInt(window.getComputedStyle(ground).getPropertyValue('bottom'));
+let groundHeight = parseInt(window.getComputedStyle(ground).getPropertyValue('height'));
+let isJumping = false;
+let upTime;
+let downTime;
+let displayScore = document.getElementById('score');
+let score = 0;
+
+function jump(){
+    if(isJumping) return;
+    upTime = setInterval(() =>{
+        if(characterBottom >= groundHeight + 250){
+            clearInterval(upTime);
+            downTime = setInterval(() => {
+                if(characterBottom <= groundHeight + 10){
+                    clearInterval(downTime);
+                    isJumping = false;
+                }
+                characterBottom -= 10;
+                character.style.bottom = characterBottom + 'px';
+            },20);
+        }
+        characterBottom += 10;
+        character.style.bottom = characterBottom + 'px';
+        isJumping = true;
+    },20);
+}
+
+function snowScore(){
+    score++;
+    displayScore.innerText = score;
+}
+
+setInterval(snowScore, 100);
+
+function generateObstacle(){
+    let obstacles = document.querySelector('.obstacles');
+    let obstacle = document.createElement('div');
+    obstacle.setAttribute('class','obstacle' );
+    obstacles.appendChild(obstacle);
+
+    let randomTimeout = Math.floor(Math.random() * 1000) +1000;
+    let obstacleRight = -30;
+    let obstacleBottom = 100;
+    let obstacleWidth = 30;
+    let obstacleHeigt = Math.floor(Math.random() * 50) + 50;
+    obstacle.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, 
+    ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+
+    function moveObstacle(){
+        obstacleRight += 5;
+        obstacle.style.right = obstacleRight + 'px'
+        obstacle.style.bottom = obstacleBottom + 'px'
+        obstacle.style.width = obstacleWidth + 'px'
+        obstacle.style.height = obstacleHeigt + 'px'
+        if(characterRight >= obstacleRight - characterWith && characterRight <= obstacleRight + obstacleWidth && characterBottom <= obstacleBottom + obstacleHeigt){
+            alert('ИГРА ОКОНЧЕНА! ВАШ СЧЕТ: '+score);
+            clearInterval(obstacleInterval);
+            clearTimeout(obstacleTimeout);
+            location.reload();
+        } 
+    }
+
+    let obstacleInterval = setInterval(moveObstacle, 20);
+    let obstacleTimeout = setTimeout(generateObstacle, randomTimeout);
+}
+
+generateObstacle();
+
+function control(e){
+    if(e.key == 'ArrowUp' || e.key == ' '){
+        jump();
+    }
+}
+
+document.addEventListener('keydown' , control);
